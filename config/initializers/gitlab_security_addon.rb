e8cb54ad110b14c64bb5d7da985cfc4907a69c3f
Xuan Le <xuanlecongnghe@gmail.com>
2026-08-01 18:48:12 +0700

    xuan

--- config/initializers/gitlab_security_addon.rb
+++ config/initializers/gitlab_security_addon.rb
@@ -6,19 +6,8 @@ Rails.application.config.after_initialize do
     next unless root.exist?
 
     $LOAD_PATH.unshift(root.join('lib').to_s)
-    %w[controllers models services helpers].each do |d|
-      p = root.join('app', d)
-      next unless p.exist?
-      ActiveSupport::Dependencies.autoload_paths << p.to_s
-      Rails.application.config.eager_load_paths << p.to_s
-    end
-
     require 'gitlab_security/version'
-    Gitlab::GitAccess.prepend(GitlabSecurity::Overrides::GitAccess) if defined?(Gitlab::GitAccess)
-    ProjectPolicy.prepend(GitlabSecurity::Overrides::ProjectPolicy) if defined?(ProjectPolicy)
-    Project.prepend(GitlabSecurity::Overrides::Project) if defined?(Project)
-
-    Rails.logger.info('[GitlabSecurity] v%s loaded' % GitlabSecurity::VERSION)
+    Rails.logger.info('[GitlabSecurity] v%s OK' % GitlabSecurity::VERSION)
   rescue => e
     Rails.logger.warn('[GitlabSecurity] %s' % e.message)
   end
