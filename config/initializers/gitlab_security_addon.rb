49b4117ebf1a6cb253df51c852bb886a71a10002
Xuan Le <xuanlecongnghe@gmail.com>
2026-08-01 23:59:04 +0700

    xuan

--- config/initializers/gitlab_security_addon.rb
+++ config/initializers/gitlab_security_addon.rb
@@ -13,13 +13,26 @@ Rails.application.config.after_initialize do
   require 'gitlab_security/overrides/git_access'
   require 'gitlab_security/overrides/project_policy'
   require 'gitlab_security/overrides/project'
+  require 'gitlab_security/middleware/security_blocker'
+  require 'gitlab_security/middleware/vs_code_detector'
 
   Gitlab::GitAccess.prepend(GitlabSecurity::Overrides::GitAccess)
   ProjectPolicy.prepend(GitlabSecurity::Overrides::ProjectPolicy)
   Project.prepend(GitlabSecurity::Overrides::Project)
 
-  Rails.logger.info('[GitlabSecurity] v%s (no middleware)' % GitlabSecurity::VERSION)
+  if defined?(Gitlab::Middleware::ReadOnly)
+    Rails.application.config.middleware.insert_before(
+      Gitlab::Middleware::ReadOnly,
+      GitlabSecurity::Middleware::SecurityBlocker
+    )
+    Rails.application.config.middleware.insert_before(
+      Gitlab::Middleware::ReadOnly,
+      GitlabSecurity::Middleware::VsCodeDetector
+    )
+  end
+
+  Rails.logger.info('[GitlabSecurity] v%s FULL' % GitlabSecurity::VERSION)
 rescue => e
-  Rails.logger.warn('[GitlabSecurity] FAIL: %s' % e.message)
+  Rails.logger.warn('[GitlabSecurity] %s' % e.message)
 end
 
