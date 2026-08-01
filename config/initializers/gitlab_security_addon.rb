a226010a1c07f612635967968db90586e86dd837
Xuan Le <xuanlecongnghe@gmail.com>
2026-08-01 22:08:53 +0700

    xuan

--- config/initializers/gitlab_security_addon.rb
+++ config/initializers/gitlab_security_addon.rb
@@ -13,23 +13,12 @@ Rails.application.config.after_initialize do
   require 'gitlab_security/overrides/git_access'
   require 'gitlab_security/overrides/project_policy'
   require 'gitlab_security/overrides/project'
-  require 'gitlab_security/middleware/security_blocker'
-  require 'gitlab_security/middleware/vs_code_detector'
 
   Gitlab::GitAccess.prepend(GitlabSecurity::Overrides::GitAccess)
   ProjectPolicy.prepend(GitlabSecurity::Overrides::ProjectPolicy)
   Project.prepend(GitlabSecurity::Overrides::Project)
 
-  Rails.application.config.middleware.insert_before(
-    Gitlab::Middleware::ReadOnly,
-    GitlabSecurity::Middleware::SecurityBlocker
-  )
-  Rails.application.config.middleware.insert_before(
-    Gitlab::Middleware::ReadOnly,
-    GitlabSecurity::Middleware::VsCodeDetector
-  )
-
-  Rails.logger.info('[GitlabSecurity] v%s FULL loaded' % GitlabSecurity::VERSION)
+  Rails.logger.info('[GitlabSecurity] v%s (no middleware)' % GitlabSecurity::VERSION)
 rescue => e
   Rails.logger.warn('[GitlabSecurity] FAIL: %s' % e.message)
 end
