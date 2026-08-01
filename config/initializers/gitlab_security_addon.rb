9baa15d6b8903fe1bbe190da3421c07c3699fe4f
Xuan Le <xuanlecongnghe@gmail.com>
2026-08-01 21:15:50 +0700

    xuan

--- config/initializers/gitlab_security_addon.rb
+++ config/initializers/gitlab_security_addon.rb
@@ -1,4 +1,11 @@
 Rails.application.config.after_initialize do
-  Rails.logger.info('[GitlabSecurity] Loaded')
+  root = Rails.root.join('gitlab_security_addon')
+  if root.exist?
+    $LOAD_PATH.unshift(root.join('lib').to_s)
+    require 'gitlab_security/version'
+    Rails.logger.info('[GitlabSecurity] v%s OK' % GitlabSecurity::VERSION)
+  end
+rescue => e
+  Rails.logger.warn('[GitlabSecurity] FAIL: %s' % e.message)
 end
 
