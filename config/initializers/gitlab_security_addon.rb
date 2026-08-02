75960ea9f5a0234f1ba0f540240127084e022e47
Xuan Le <xuanlecongnghe@gmail.com>
2026-08-02 15:52:01 +0700

    xuan

--- config/initializers/gitlab_security_addon.rb
+++ config/initializers/gitlab_security_addon.rb
@@ -1,14 +1,3 @@
-Rails.application.config.after_initialize do
-  root = Rails.root.join('gitlab_security_addon')
-  next unless root.exist?
-  lib = root.join('lib')
-  $LOAD_PATH.unshift(lib.to_s)
+# No overrides - baseline test
 
-  require 'gitlab_security/version'
-  require 'gitlab_security/overrides/git_access'
-  Gitlab::GitAccess.prepend(GitlabSecurity::Overrides::GitAccess)
-  Rails.logger.info('[GitlabSecurity] +GitAccess only')
-rescue => e
-  Rails.logger.warn('[GitlabSecurity] %s' % e.message)
-end
 
