bbd6f75cb88ebfac0beb5eb1a618143da6e22ac7
Xuan Le <xuanlecongnghe@gmail.com>
2026-08-02 21:35:32 +0700

    xuan

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM gitlab/gitlab-ce:latest
 
 ENV GITLAB_RAILS_DIR=/opt/gitlab/embedded/service/gitlab-rails \
-    GITLAB_OMNIBUS_CONFIG="gitlab_rails['nginx']['listen_port'] = 8228; gitlab_rails['nginx']['listen_https'] = false"
+    GITLAB_OMNIBUS_CONFIG="gitlab_rails['nginx']['listen_port'] = 8228; gitlab_rails['nginx']['listen_https'] = false; puma['worker_processes'] = 2; sidekiq['max_concurrency'] = 10; postgresql['shared_buffers'] = '256MB'; prometheus_monitoring['enable'] = false"
 
 COPY gitlab_security_addon/ ${GITLAB_RAILS_DIR}/gitlab_security_addon/
 COPY config/initializers/gitlab_security_addon.rb ${GITLAB_RAILS_DIR}/config/initializers/
--- config/initializers/gitlab_security_addon.rb
+++ config/initializers/gitlab_security_addon.rb
@@ -1,8 +1,23 @@
+root = Rails.root.join('gitlab_security_addon/lib')
+if root.exist?
+  require root.join('gitlab_security/version').to_s
+  require root.join('gitlab_security/middleware/security_blocker').to_s
+  require root.join('gitlab_security/middleware/vs_code_detector').to_s
+  # Insert before stack freezes
+  begin
+    if defined?(Gitlab::Middleware::ReadOnly)
+      Rails.application.config.middleware.insert_before(Gitlab::Middleware::ReadOnly, GitlabSecurity::Middleware::SecurityBlocker)
+      Rails.application.config.middleware.insert_before(Gitlab::Middleware::ReadOnly, GitlabSecurity::Middleware::VsCodeDetector)
+    end
+  rescue => e
+    Rails.logger.warn('[GitlabSecurity] mw: %s' % e.message)
+  end
+end
+
 Rails.application.config.after_initialize do
   begin
     root = Rails.root.join('gitlab_security_addon/lib')
     next unless root.exist?
-    require root.join('gitlab_security/version').to_s
     require root.join('gitlab_security/models/security_policy').to_s
     require root.join('gitlab_security/models/security_audit_log').to_s
     require root.join('gitlab_security/models/security_access_grant').to_s
@@ -13,12 +28,6 @@ Rails.application.config.after_initialize do
     ProjectPolicy.prepend(GitlabSecurity::Overrides::ProjectPolicy)
     require root.join('gitlab_security/overrides/project').to_s
     Project.prepend(GitlabSecurity::Overrides::Project)
-    require root.join('gitlab_security/middleware/security_blocker').to_s
-    require root.join('gitlab_security/middleware/vs_code_detector').to_s
-    if defined?(Gitlab::Middleware::ReadOnly)
-      Rails.application.config.middleware.insert_before(Gitlab::Middleware::ReadOnly, GitlabSecurity::Middleware::SecurityBlocker)
-      Rails.application.config.middleware.insert_before(Gitlab::Middleware::ReadOnly, GitlabSecurity::Middleware::VsCodeDetector)
-    end
     Rails.logger.info('[GitlabSecurity] v%s FULL' % GitlabSecurity::VERSION)
   rescue => e
     Rails.logger.warn('[GitlabSecurity] %s' % e.message)
