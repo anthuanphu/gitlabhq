62b20675ba9ce732ef109d55a3bd0fedc7cfc2f9
Xuan Le <xuanlecongnghe@gmail.com>
2026-08-02 18:09:29 +0700

    test: load path only - no models

--- config/initializers/gitlab_security_addon.rb
+++ config/initializers/gitlab_security_addon.rb
@@ -1,15 +1,9 @@
 Rails.application.config.after_initialize do
   root = Rails.root.join('gitlab_security_addon')
-  next unless root.exist?
-  $LOAD_PATH.unshift(root.join('lib').to_s)
-  require 'gitlab_security/version'
-  require 'gitlab_security/models/security_policy'
-  require 'gitlab_security/models/security_audit_log'
-  require 'gitlab_security/models/security_access_grant'
-  require 'gitlab_security/models/device_whitelist'
-  require 'gitlab_security/overrides/git_access'
-  Gitlab::GitAccess.prepend(GitlabSecurity::Overrides::GitAccess)
-  Rails.logger.info('[GitlabSecurity] +GitAccess')
+  if root.exist?
+    $LOAD_PATH.unshift(root.join('lib').to_s)
+  end
+  Rails.logger.info('[GitlabSecurity] load path only')
 rescue => e
   Rails.logger.warn('[GitlabSecurity] %s' % e.message)
 end
