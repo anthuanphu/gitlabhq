21fc9f64e135ab3125c4050312267d11922178f3
Xuan Le <xuanlecongnghe@gmail.com>
2026-08-02 14:52:20 +0700

    xuan

--- config/initializers/gitlab_security_addon.rb
+++ config/initializers/gitlab_security_addon.rb
@@ -1,5 +1,18 @@
 Rails.application.config.after_initialize do
-  Rails.logger.info('[GitlabSecurity] Minimal - no overrides')
-end
+  root = Rails.root.join('gitlab_security_addon')
+  next unless root.exist?
+  lib = root.join('lib')
+  $LOAD_PATH.unshift(lib.to_s)
 
+  require 'gitlab_security/version'
+  require 'gitlab_security/models/security_policy'
+  require 'gitlab_security/models/security_audit_log'
+  require 'gitlab_security/models/security_access_grant'
+  require 'gitlab_security/models/device_whitelist'
+  require 'gitlab_security/overrides/git_access'
+  Gitlab::GitAccess.prepend(GitlabSecurity::Overrides::GitAccess)
+  Rails.logger.info('[GitlabSecurity] +GitAccess')
+rescue => e
+  Rails.logger.warn('[GitlabSecurity] %s' % e.message)
+end
 
