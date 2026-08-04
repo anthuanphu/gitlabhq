89faf063567fe2bc77894e0c83e8ce0babe1454a
Xuan Le <xuanlecongnghe@gmail.com>
2026-08-04 21:53:41 +0700

    fix: xuan

--- config/initializers/gitlab_security_addon.rb
+++ config/initializers/gitlab_security_addon.rb
@@ -1,27 +1,30 @@
 # frozen_string_literal: true
 
-# Auto-create table + inject admin security UI
-Rails.application.config.to_prepare do
-  # 1. Create table
-  begin
-    unless ActiveRecord::Base.connection.table_exists?(:project_security_settings)
-      ActiveRecord::Base.connection.create_table :project_security_settings, if_not_exists: true do |t|
-        t.bigint :project_id, null: false; t.index :project_id, unique: true
-        t.boolean :allow_clone, default: false, null: false
-        t.boolean :allow_download, default: false, null: false
-        t.boolean :allow_fork, default: false, null: false
-        t.boolean :allow_export, default: false, null: false
-        t.boolean :allow_ide_access, default: false, null: false
-        t.boolean :enabled, default: true, null: false
-        t.timestamps_with_timezone null: false
+# Source Code Protection — table + admin UI inject
+Rails.application.config.after_initialize do
+  # 1. Table (lazy, after PG ready)
+  Thread.new do
+    sleep 5
+    begin
+      unless ActiveRecord::Base.connection.table_exists?(:project_security_settings)
+        ActiveRecord::Base.connection.create_table :project_security_settings, if_not_exists: true do |t|
+          t.bigint :project_id, null: false; t.index :project_id, unique: true
+          t.boolean :allow_clone, default: false, null: false
+          t.boolean :allow_download, default: false, null: false
+          t.boolean :allow_fork, default: false, null: false
+          t.boolean :allow_export, default: false, null: false
+          t.boolean :allow_ide_access, default: false, null: false
+          t.boolean :enabled, default: true, null: false
+          t.timestamps_with_timezone null: false
+        end
+        Rails.logger.info('[SourceProtection] Table created')
       end
-      Rails.logger.info('[SourceProtection] Table created')
+    rescue => e
+      Rails.logger.warn('[SourceProtection] %s' % e.message)
     end
-  rescue => e
-    Rails.logger.warn('[SourceProtection] %s' % e.message)
   end
 
-  # 2. Inject @security_setting into Admin::ProjectsController#show
+  # 2. Inject admin UI
   Admin::ProjectsController.prepend(Module.new do
     def show
       @security_setting = begin
@@ -31,8 +34,7 @@ Rails.application.config.to_prepare do
         else
           @project.build_security_setting
         end
-      rescue => e
-        Rails.logger.warn('[SourceProtection] %s' % e.message)
+      rescue
         @project.build_security_setting
       end
       super
@@ -40,20 +42,19 @@ Rails.application.config.to_prepare do
 
     def update_security
       ProjectSecuritySetting.ensure_table!
-      setting = begin
+      s = begin
         if ActiveRecord::Base.connection.table_exists?(:project_security_settings)
           @project.security_setting || @project.build_security_setting
         else
           @project.build_security_setting
         end
       end
-      setting.assign_attributes(
-        params.require(:project_security_setting).permit(:allow_clone, :allow_download, :allow_fork, :allow_export, :allow_ide_access, :enabled)
-      )
-      if setting.save
+      s.assign_attributes(params.require(:project_security_setting).permit(
+        :allow_clone, :allow_download, :allow_fork, :allow_export, :allow_ide_access, :enabled))
+      if s.save
         redirect_to admin_project_path(@project), notice: 'Security settings updated.'
       else
-        redirect_to admin_project_path(@project), alert: setting.errors.full_messages.join(', ')
+        redirect_to admin_project_path(@project), alert: s.errors.full_messages.join(', ')
       end
     end
   end) rescue nil
